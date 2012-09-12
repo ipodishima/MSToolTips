b01649df173c868ad0529e2655a186d2ec0af6b2
Marian PAUL <marian.paul2@gmail.com>
2012-09-12 10:21:26 +0200

    Initial Commit

---
+++ MSToolTips/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MSToolTips
+//
+//  Created by Marian Paul on 12/09/12.
+//  Copyright (c) 2012 Marian Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ MSToolTips/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MSToolTips
+//
+//  Created by Marian Paul on 12/09/12.
+//  Copyright (c) 2012 Marian Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
