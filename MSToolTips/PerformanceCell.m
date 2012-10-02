b7e51fb75480c16b05fc928d1bba36f91f491b61
Marian PAUL <marian.paul2@gmail.com>
2012-10-02 14:16:29 +0200

    Added Files for Performance Debug part

---
+++ MSToolTips/PerformanceCell.m
@@ -0,0 +1,29 @@
+//
+//  PerformanceCell.m
+//  MSToolTips
+//
+//  Created by Marian Paul on 02/10/12.
+//  Copyright (c) 2012 Marian Paul. All rights reserved.
+//
+
+#import "PerformanceCell.h"
+
+@implementation PerformanceCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ MSToolTips/PerformancesViewController.h
@@ -0,0 +1,13 @@
+//
+//  PerformancesViewController.h
+//  MSToolTips
+//
+//  Created by Marian Paul on 02/10/12.
+//  Copyright (c) 2012 Marian Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PerformancesViewController : UITableViewController
+
+@end
